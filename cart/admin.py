855f52e3b5a1247c1d8ebee869bb42f2192bfa5f
mentesnot-2 <sibhatmentesnot@gmail.com>
2025-05-18 01:09:13 +0300

    cart added

---
+++ cart/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Cart, CartItem
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'total_items', 'total_price', 'updated_at')
+    inlines = [CartItemInline]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity', 'total_price', 'updated_at')
+    list_filter = ('cart__user',)
+    search_fields = ('product__name', 'cart__user__email') 
